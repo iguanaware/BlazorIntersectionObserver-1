dd1f703d8e680d9e6d0713a1b1276ae5db48d4b2
Brian Freeman <brian.freeman@darkmatter2bd.com>
2022-09-08 09:43:09 -0400

    MedFuse Updates

---
+++ BuildNuGets.ps1
@@ -0,0 +1,23 @@
+﻿#$versionsuffix = "v20220908"
+#in project file
+
+Set-Location $PSScriptRoot;
+$nugetoutput = "$PSScriptRoot\nuget"
+Remove-Item -LiteralPath $nugetoutput -Recurse -ErrorAction SilentlyContinue
+
+$projecttargets = @("Ljbc1994.Blazor.IntersectionObserver");
+$projects = Get-ChildItem -Filter *.csproj -Recurse `
+    | Where-Object { $projecttargets -Contains $_.BaseName};
+
+$projects | Select BaseName | Format-Table
+
+$p = $projects;
+
+
+foreach ($p in $projects){
+    dotnet pack "$($p.FullName)" --force --include-source --include-symbols --interactive --output nuget --verbosity  m   #--version-suffix "$versionsuffix"
+}
+
+#dotnet nuget push --source "TestNuget" --api-key az <package-path>
+return
+
---
+++ PublishNuGets.ps1
@@ -0,0 +1,34 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+
+#Warning you should not have duplicate!! Only for experts
+$skipduplicates  = $false
+#-------------------------------------------------------
+$production = "https://pkgs.dev.azure.com/darkmatter2bd/NuGet/_packaging/Production/nuget/v3/index.json"
+$testing = "https://pkgs.dev.azure.com/darkmatter2bd/NuGet/_packaging/TestNuget/nuget/v3/index.json"
+
+$nugetsource = $production;
+Set-Location $PSScriptRoot;
+$nugetoutput = "$PSScriptRoot\nuget"
+$nugets = Get-ChildItem -LiteralPath $nugetoutput -Filter *.nupkg | Where-Object BaseName -Like "*.symbols";
+$nugets | Select BaseName | Format-Table
+
+function ThrowOnNativeFailure {
+    if (-not $?)
+    {
+        throw 'Native Failure'
+    }
+}
+
+$skipdups = "";
+if($skipduplicates) {
+$skipdups += "--skip-duplicate";
+}
+
+foreach ($p in $nugets){
+   dotnet nuget push --source `"$nugetsource`" --interactive $skipdups --api-key az `"$($p.FullName)`" ;
+   ThrowOnNativeFailure;
+}
+
+
